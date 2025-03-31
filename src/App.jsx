d6aaf008407e719394c21249bd868935ccbc420f
tsztin0217 <tsztin0217@gmail.com>
2025-03-31 01:07:56 -0400

    Import components and define routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import NavBar from './components/NavBar/NavBar';
 import MailboxDetails from './components/MailboxDetails/MailboxDetails';
-import './App.css'
-import {Route, Routes} from 'react-router';
+import MailboxList from './components/MailboxList/MailboxList';
+import MailboxForm from './components/MailboxForm/MailboxForm';
+import { Routes, Route } from 'react-router';
+import './index.css';
+
 
 
 
@@ -11,8 +14,36 @@ const App = () => {
 
   const addBox = (newFormData) => {
     newFormData._id = mailboxes.length + 1;
-    setMailboxes(mailboxes, newFormData);
+    setMailboxes([...mailboxes, newFormData]);
   }
+
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route
+          path='/'
+          element={<main><h1>Post Office</h1></main>}
+        />
+        <Route
+          path='/mailboxes'
+          element={<MailboxList mailboxes={mailboxes}/>}
+        />
+        <Route
+          path='/new-mailbox'
+          element={<MailboxForm addBox={addBox}/>}
+        />
+        <Route
+          path='/mailboxes/:mailboxId'
+          element={<MailboxDetails mailboxes={mailboxes}/>}
+        />
+        <Route
+          path='*'
+          element={<h2>Nothing here!</h2>}
+        />
+      </Routes>
+    </>
+  )
 };
 
 export default App;
