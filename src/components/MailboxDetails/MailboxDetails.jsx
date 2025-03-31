d17057af8a2ff19dfc9f42abeb534eaaf195bfd9
tsztin0217 <tsztin0217@gmail.com>
2025-03-31 01:21:19 -0400

    Display individual mailbox details

--- src/components/MailboxDetails/MailboxDetails.jsx
+++ src/components/MailboxDetails/MailboxDetails.jsx
@@ -1,12 +1,26 @@
 import { useParams } from 'react-router';
 
 
-const MailboxDetails = { props } => {
+const MailboxDetails = (props) => {
     const { mailboxId } = useParams();
 
     const selectedBox = props.mailboxes.find(
-        (mailbox) => mailbox._id === Number(mailboxId)
+        (mailbox) => mailbox._id === parseInt(mailboxId)
     );
+
+    return (
+        <>
+            <h1>Mailbox {mailboxId}</h1>
+            <h2>Details</h2>
+            <p>
+                Boxholder: {selectedBox.boxOwner}
+            </p>
+            <p>
+                Box Size: {selectedBox.boxSize}
+            </p>
+
+        </>
+    )
 
 }
 
